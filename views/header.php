08203fcf70397d0a1ec3d97340611c0e955d8c07
Will Anderson <will@itsananderson.com>
2013-02-26 05:46:28 -0800

    Add an HTML title tag and increase the height of textareas

--- views/header.php
+++ views/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<title>WordPress to GitHub Markdown Converter</title>
 		<link rel="stylesheet" href="<?php echo WP2MD_ROOT_URL; ?>resources/wp2md.css" />
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 		<script type="text/javascript" src="<?php echo WP2MD_ROOT_URL; ?>resources/wp2md.js"></script>
