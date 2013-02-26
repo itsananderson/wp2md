b825396da6fe89f6e4c642eab06901bc55a4919d
Will Anderson <will@itsananderson.com>
2013-02-26 05:07:27 -0800

    Add a new WP2MD_ROOT_URL constant that can be overridden

--- constants.php
+++ constants.php
@@ -2,6 +2,10 @@
 
 define( 'WP2MD_ROOT', dirname( __FILE__ ) );
 
+if ( !defined( 'WP2MD_ROOT_URL' ) ) {
+	define( 'WP2MD_ROOT_URL', './' );
+}
+
 if ( !defined( 'WP2MD_AUTORUN' ) ) {
 	define( 'WP2MD_AUTORUN', false );
 }
--- views/header.php
+++ views/header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" href="resources/wp2md.css" />
+		<link rel="stylesheet" href="<?php echo WP2MD_ROOT_URL; ?>resources/wp2md.css" />
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-		<script type="text/javascript" src="resources/wp2md.js"></script>
+		<script type="text/javascript" src="<?php echo WP2MD_ROOT_URL; ?>resources/wp2md.js"></script>
 	</head>
 	<body>
 		<div id="main">
