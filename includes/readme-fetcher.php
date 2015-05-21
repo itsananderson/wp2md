ebe438972454a47e2bafb7647d8a597b21a0df07
Will Anderson <will@itsananderson.com>
2015-05-20 23:07:52 -0700

    Follow redirects and turn off SSL validation

--- includes/readme-fetcher.php
+++ includes/readme-fetcher.php
@@ -17,6 +17,8 @@ class Readme_Fetcher {
 			$curl = curl_init();
 			curl_setopt( $curl, CURLOPT_URL, $url );
 			curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+			curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
 			$response = curl_exec( $curl );
 
